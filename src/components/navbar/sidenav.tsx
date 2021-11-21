ff9c13d6878c9179e7dc3f7ee8dec1e8e1142562
Dominik Blau <dominik.blau@gmail.com>
2021-11-21 19:50:36 +0100

    fixed eslint issues

--- src/components/navbar/sidenav.tsx
+++ src/components/navbar/sidenav.tsx
@@ -1,6 +1,4 @@
 import React, { createRef } from "react";
-import CloseIcon from '@mui/icons-material/Close';
-import MenuIcon from '@mui/icons-material/Menu';
 
 import menuJson from '@data/menu.json';
 
--- src/components/skill/skill.tsx
+++ src/components/skill/skill.tsx
@@ -10,9 +10,8 @@ export default  class Skill extends React.Component<{}> {
     render() {
         const renderSkills = [];
         for (const [key, value] of Object.entries(skillsJson)) {
-            // renderSkills.push(<div key={key} className="skill-headline">{key}</div>);
             renderSkills.push(value.map((skill: ISkill, index: number) => {
-                    return <div key={index} className="skill">
+                    return <div key={key+index} className="skill">
                         <Card sx={{ flex: 1, marginBottom: 1 }}>
                             <CardActionArea>
                                 <CardContent>
@@ -43,43 +42,6 @@ export default  class Skill extends React.Component<{}> {
                     </div>
             }));
         }
-        // const renderSkills = skillsJson.skills.map((category: Array<ISkill>) => {
-        //     return category.map((skill: ISkill, idx) => {
-        //         return {skill.label};
-        //     });
-        // });
-
-        // const renderSkills = skillsJson.skills.map((skill, index) => {
-        //     return <div key={index} className="skill">
-        //                 <Card sx={{ flex: 1, marginBottom: 1 }}>
-        //                     <CardActionArea>
-        //                         <CardContent>
-        //                             <Box sx={{ display: 'flex' }}>
-        //                                 <Box className="label" sx={{ alignSelf: 'flex-start', flex: 1, marginRight: 10 }}>
-        //                                     <Typography
-        //                                         variant="body2"
-        //                                         component="div"
-        //                                         align="left">
-        //                                         {skill.label}
-        //                                     </Typography>
-        //                                 </Box>
-        //                                 <Box sx={{ marginLeft: 'auto' }}>
-        //                                     <Typography
-        //                                         variant="body2"
-        //                                         component="div">
-        //                                         {skill.percent}
-        //                                     </Typography>
-        //                                 </Box>
-        //                             </Box>
-        //                             <LinearProgress
-        //                                 color="primary"
-        //                                 variant="determinate"
-        //                                 value={75} />
-        //                         </CardContent>
-        //                     </CardActionArea>
-        //                 </Card>
-        //             </div>
-        // });
         return (        
             <Box sx={{ display: 'flex', flexFlow: 'row', flexWrap: 'wrap', justifyContent: 'center'}}>
                 {renderSkills}
